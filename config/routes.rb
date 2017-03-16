37c6f738f014418004c7317c4da8d9e3a98f43e3
gaoios <gaoios@foxmail.com>
2017-03-16 22:19:11 +0800

    move sqlite3 to dev grouup & add pg to production group

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  root 'topics#index'
   resources :topics
   resources :articles
   get 'welcome/index'
   get 'articles/new'
-  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
